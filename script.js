6cabf7be7bff031a9602b981f7b145a5c2c98c66
Lola <mledun@hotmail.com>
2021-05-03 18:42:01 +0000

    added multiplication function

--- script.js
+++ script.js
@@ -29,6 +29,9 @@ function runGame(gameType) {
 
      if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
+    }
+    else if (gameType === "multiply") {
+        displayMultiplyQuestion(num1, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type ${gameType}, aborting`;
@@ -70,6 +73,9 @@ function calculateRightAnswer() {
     if (operator === "+") { // This is the addition game
         return [operand1 + operand2, "addition"]; // return an array containing the correct answer and game type
     }
+    else if (operator === "x") { // This is the subtraction game
+        return [operand1 * operand2, "multiply"]; // return an array containing the correct answer and game type
+    }
     else {
         alert(`Unimplemented operator: ${operator}`);
         throw `Unimplemented operator ${operator}, aborting`;
@@ -107,8 +113,10 @@ function displaySubtractQuestion() {
 
 }
 
-function displayMultiplyQuestion() {
-
+function displayMultiplyQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "x";
 }
 
 function displayDivisionQuestion() {
